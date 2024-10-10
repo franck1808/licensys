43cb14f2bc8d140dfeab53c44ec4f23109e0f157
franck1808 <franck.tchomegni@nextsolutions-labs.com>
2024-10-10 10:35:01 +0100

    First commit 2

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.app')
+@section('body-content')
+    <div class="container-xxl container-p-y">
+        <center>
+            <div class="misc-wrapper">
+                <h2 class="mb-2 mx-2">Page Not Found :(</h2>
+                <p class="mb-4 mx-2">Oops! 😖 The requested URL was not found on this server.</p>
+                <a href="{{ route('dashboard') }}" class="btn btn-primary">Back to home</a>
+                <div class="mt-3">
+                    <img
+                    src="{{ asset('app-assets/img/illustrations/page-misc-error-light.png') }}"
+                    alt="page-misc-error-light"
+                    width="500"
+                    class="img-fluid"
+                    data-app-dark-img="illustrations/page-misc-error-dark.png"
+                    data-app-light-img="illustrations/page-misc-error-light.png"
+                    />
+                </div>
+            </div>
+        </center>
+    </div>
+@endsection
